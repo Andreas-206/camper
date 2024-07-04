cc9d26ed43e205f88b268d970bb39797e18ecc39
Andreas-206 <puma_real@ukr.net>
2024-07-04 16:03:38 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = ''
+axios.defaults.baseURL = 'https://66852ad0b3f57b06dd4ba5b9.mockapi.io/advert'
 
 export const getCampers = async (page = 1) => {
 const {data} = await axios.get(`/adverts?page=${page}&limit=4`)
