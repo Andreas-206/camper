41cae5960fb76ee250a63d072404c7afc0f80153
Andreas-206 <puma_real@ukr.net>
2024-07-05 15:52:14 +0300

    +

--- src/page/FavoritesPage.styled.js
+++ src/page/FavoritesPage.styled.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const Message = styled.p`
+  width: 800px;
+  margin: 150px auto;
+  border: 4px solid var(--color-button)
+  border-radius: 20px;
   font-size: 24px;
   font-weight: 600;
-  color: var(--color-button);
+  color: var(--color-main);
   text-align: center;
   margin-top: 100px;
 `;
