d40d91d35673b3e06eb9dff867a88bbdceafecca
Andreas-206 <puma_real@ukr.net>
2024-07-07 14:17:21 +0300

    Revert "Update CatalogPage.jsx"
    This reverts commit 7958c49317c9a22fc8ccdc1a0425cf4a63a3113e.

--- src/page/CatalogPage.jsx
+++ src/page/CatalogPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AdvertsList } from '../components/AdvertsList/AdvertsList';
 import { AdvertsFilter } from '../components/AdvertsFilter/AdvertsFilter';
