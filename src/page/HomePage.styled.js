7f8bf701034136552ad80f7948829a8d6b11b461
Andreas-206 <puma_real@ukr.net>
2024-07-07 13:54:21 +0300

    +

--- src/page/HomePage.styled.js
+++ src/page/HomePage.styled.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const HomeMessage = styled.h1`
-  width: 600px;
+  width: 800px;
   margin: 150px auto;
-  color: var(--color-button);
+  border: 4px solid var(--color-button);
+  border-radius: 200px;
+  padding: 20px;
+  color: var(--color-main);
 `;
